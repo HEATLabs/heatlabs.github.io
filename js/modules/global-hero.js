d90d200cf4e663da21c84b6a4e47da7027c171a8
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-05-24 01:51:08 +0300

    tweaked global hero script to use the new hero images

--- js/modules/global-hero.js
+++ js/modules/global-hero.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Only proceed if we found hero elements
     if (heroes.length > 0) {
         // Generate a random number between 1 and 46
-        const randomImageNumber = Math.floor(Math.random() * 43) + 1;
+        const randomImageNumber = Math.floor(Math.random() * 47) + 1;
 
         // Construct the new image URL
         const newImageUrl = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://raw.githubusercontent.com/PCWStats/Website-Images/refs/heads/main/hero-background/${randomImageNumber}.webp')`;
