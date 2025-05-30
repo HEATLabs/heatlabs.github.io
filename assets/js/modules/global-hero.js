583f8f6aa8cf4de184272ba088b5fb28f4dc3f26
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-05-30 08:52:45 +0300

    updated global hero module to use the new 48th hero image

--- assets/js/modules/global-hero.js
+++ assets/js/modules/global-hero.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Only proceed if we found hero elements
     if (heroes.length > 0) {
         // Generate a random number between 1 and 46
-        const randomImageNumber = Math.floor(Math.random() * 47) + 1;
+        const randomImageNumber = Math.floor(Math.random() * 48) + 1;
 
         // Construct the new image URL
         const newImageUrl = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('https://cdn.jsdelivr.net/gh/PCWStats/Website-Images@main/hero-background/${randomImageNumber}.webp')`;
