8848f193abfcfd5a0aff69ca3196e0aa9aab123c
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-10-15 08:17:15 +0300

    removed assets folder

--- assets/js/pages/players.js
+++
@@ -1,3 +0,0 @@
-// PLACEHOLDER
-const PLAYERS_JSON_PATH = 'https://cdn.jsdelivr.net/gh/HEATLabs/HEAT-Labs-Database@main/mock-api/players.json';
-const CLANS_JSON_PATH = 'https://cdn.jsdelivr.net/gh/HEATLabs/HEAT-Labs-Database@main/mock-api/clans.json';
